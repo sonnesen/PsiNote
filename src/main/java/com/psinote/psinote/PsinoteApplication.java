4342731be746c62727b817ac9257cce1b314650f
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2024-08-13 15:44:24 -0300

    init

---
+++ src/main/java/com/psinote/psinote/PsinoteApplication.java
@@ -0,0 +1,13 @@
+package com.psinote.psinote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsinoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsinoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psinote/psinote/PsinoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psinote.psinote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsinoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
