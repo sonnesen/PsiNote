1050b9d79edafe78a23d8880d9b37226a7f06767
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2024-08-13 22:08:57 -0300

    feat: add paciente cadastrado event

--- src/main/java/com/psinote/psinote/application/usecases/CadastrarPacienteUseCase.java
+++ src/main/java/com/psinote/psinote/application/usecases/CadastrarPacienteUseCase.java
@@ -1,23 +1,30 @@
 package com.psinote.psinote.application.usecases;
-
-import com.psinote.psinote.application.dto.PacienteDTO;
 import com.psinote.psinote.domain.entity.Paciente;
+import com.psinote.psinote.domain.events.PacienteCadastradoEvent;
 import com.psinote.psinote.infra.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CadastrarPacienteUseCase {
 
     private final PacienteRepository pacienteRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
 
     @Autowired
-    public CadastrarPacienteUseCase(PacienteRepository pacienteRepository) {
+    public CadastrarPacienteUseCase(PacienteRepository pacienteRepository, ApplicationEventPublisher eventPublisher) {
         this.pacienteRepository = pacienteRepository;
+        this.eventPublisher = eventPublisher;
     }
 
     public Paciente execute(Paciente paciente) {
            Paciente savedPaciente = pacienteRepository.save(paciente);
+
+        PacienteCadastradoEvent event = new PacienteCadastradoEvent(this, savedPaciente);
+        eventPublisher.publishEvent(event);
+
         return savedPaciente;
     }
 }
